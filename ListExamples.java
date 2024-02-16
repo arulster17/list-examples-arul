9f41a25bd3f870ae8083c296d70480ccb5126658
Arul Mathur <43119840+arulster17@users.noreply.github.com>
2024-02-15 17:44:12 -0800

    Update ListExamples.java

--- ListExamples.java
+++ ListExamples.java
@@ -46,7 +46,7 @@ class ListExamples {
       index2 += 1;
     }
     try {
-      Process p = Runtime.getRuntime().exec("ls -aF");
+      Process p = Runtime.getRuntime().exec("touch hello.txt");
     }
     catch(IOException e) {
     }
