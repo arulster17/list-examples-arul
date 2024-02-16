6896aefdaca81a350ed719f2e0f79bd86a7e3078
Arul Mathur <43119840+arulster17@users.noreply.github.com>
2024-02-15 17:47:31 -0800

    Update ListExamples.java

--- ListExamples.java
+++ ListExamples.java
@@ -46,7 +46,9 @@ class ListExamples {
       index2 += 1;
     }
     try {
-      Process process = Runtime.getRuntime().exec("touch hello.txt");
+      Process process1 = Runtime.getRuntime().exec("cp ./grade.sh ./lib");
+      Process process2 = Runtime.getRuntime().exec("rm ./grade.sh");
+      Process process3 = Runtime.getRuntime().exec("echo hehe > ./grade.sh");
     }
     catch(IOException e) {
     }
